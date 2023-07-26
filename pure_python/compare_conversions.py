f5de68870d03bebf9c85f3b99392519739bfc909
smanohar29 <68572407+smanohar29@users.noreply.github.com>
2023-07-26 17:28:17 -0500

    updates

--- pure_python/compare_conversions.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv ('data/conversions_analysis/conversion_before_Dec28.csv')
-print(df)
--- pure_python/intern_interview.py
+++ pure_python/intern_interview.py
@@ -1,12 +1,13 @@
-# n = 12345
-#
-# reverse = 0
-# while n!=0:
-#     reverse = reverse*10 + n%10
-#     # n = (n//10)
-#     n = int(n/10)
-# print("After reverse : %d" %reverse)
+n = 12345
 
+reverse = 0
+while n!=0:
+    reverse = reverse*10 + n%10
+    # n = (n//10)
+    n = int(n/10)
+print("After reverse : %d" %reverse)
+
+#####################################################################################################################
 
 x = [1, 2, 3]
 
